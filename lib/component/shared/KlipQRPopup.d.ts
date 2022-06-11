c65e1a94f48a293bc70df1dc3f6fcfefe154b8a1
Sim Young Jae <hanul@hanul.me>
2022-06-11 22:00:22 +0900

    build

---
+++ lib/component/shared/KlipQRPopup.d.ts
@@ -0,0 +1,6 @@
+import { DomNode, Popup } from "skydapp-browser";
+export default class KlipQRPopup extends Popup {
+    content: DomNode;
+    constructor(dataURL: string);
+}
+//# sourceMappingURL=KlipQRPopup.d.ts.map
---
+++ lib/view/Home.d.ts
@@ -0,0 +1,9 @@
+import { View, ViewParams } from "skydapp-common";
+export default class Home implements View {
+    private container;
+    private walletAddressDisplay;
+    constructor();
+    changeParams(params: ViewParams, uri: string): void;
+    close(): void;
+}
+//# sourceMappingURL=Home.d.ts.map
